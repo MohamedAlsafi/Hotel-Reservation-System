ba189ad454fc279c35d6e88cb242befdd81d75ec
MohamedAlsafi <mohamed.m.alsafi@gmail.com>
2025-03-01 01:05:12 +0200

    modification

--- Hotel.Core/Entities/Rooms/Room.cs
+++ Hotel.Core/Entities/Rooms/Room.cs
@@ -16,10 +16,10 @@ namespace Hotel.Core.Entities.Rooms
         public decimal Price { get; set; }
 
         public bool IsAvailable { get; set; } = true;
-        public ICollection<RoomFacility> Facilities { get; set; } = new List<RoomFacility>();
+        public ICollection<RoomFacility> RoomFacilities { get; set; } = new List<RoomFacility>();
         public ICollection<RoomImage> Images { get; set; } = new List<RoomImage>();
         public ICollection<RoomStaff> RoomStaff { get; set; } = new HashSet<RoomStaff>();
-       // public ICollection<Reservation> Reservations { get; set; }
+        public ICollection<Reservation> Reservations { get; set; } = new HashSet<Reservation>();
         public ICollection<RoomOffer> Offers { get; set; } = new HashSet<RoomOffer>();
     }
 }
