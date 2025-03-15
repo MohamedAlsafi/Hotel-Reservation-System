848e25f6ffc4a795b85bc0105919982dc2c796e9
MohamedAlsafi <mohamed.m.alsafi@gmail.com>
2025-03-15 14:26:41 +0200

    Remove IsAvaible From Room

---
+++ Hotel.Core/Migrations/20250315122315_Remove_IsAvailable_From_Room.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_IsAvailable_From_Room : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAvailable",
+                table: "Room");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvailable",
+                table: "Room",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
