c2b67f60aca01f18f5deda787ac782fc72504ff6
mohamedahmed11233 <ma5395822@gmail.com>
2025-04-12 22:06:33 +0200

    test

--- Hotel.Repository/Services/PasswordHashing/PasswordHasher.cs
+++ Hotel.Repository/Services/PasswordHashing/PasswordHasher.cs
@@ -8,12 +8,10 @@ namespace Hotel.Repository.Services.PasswordHashing
         private readonly int SaltSize = 16;
         private readonly int HashSize = 20;
         private readonly int Iterations = 10000;
-        // Use SHA256 which is widely supported across platforms
         private static readonly HashAlgorithmName hashAlgorithmName = HashAlgorithmName.SHA256;
 
         public string HashPassword(string password)
         {
-            // Use a cryptographically secure random number generator
             byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
 
             // Use the SHA256 algorithm which is widely supported
